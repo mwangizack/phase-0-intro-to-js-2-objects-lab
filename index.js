bcc4f65950ec36da3dd2c0a940d557328b8e84ef
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-03-19 20:57:28 +0300

    Add functions to index,js to manipulate the employee object

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Zack Mwangi",
+    streetAddress: "Ruaka, Kiambu",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employeeCopy = {...employee};
+    employeeCopy[key] = value;
+    return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeCopy = {...employee};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
